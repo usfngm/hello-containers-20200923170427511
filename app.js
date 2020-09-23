227d90036088eb36d36b93a39cc89cbf922e7ced
Youssef Negm <usf.ngm@gmail.com>
2020-09-23 19:34:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ if (process.env.PORT) {
 
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Welcome to IBM Cloud DevOps using containers. Let\'s go use the Continuous Delivery Service');
+  res.send('This is my first update');
 });
 
 app.listen(PORT);
